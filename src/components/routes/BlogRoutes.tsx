9c0589f33b6d9207b87832c6e62b8fb0ce5573b9
lizardspace <154006119+lizardspace@users.noreply.github.com>
2025-05-10 12:57:48 +0000

    ffffffffffffffdd

--- src/components/routes/BlogRoutes.tsx
+++ src/components/routes/BlogRoutes.tsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ArticleCreationForm from '@/pages/admin/articles/ArticleCreationForm';
+import CategoryCreationForm from '@/pages/admin/articles/CategoryCreationForm';
 
 const BlogRoutes = () => {
   return (
     <Routes>
       <Route index element={<ArticleCreationForm />} />
       <Route path="nouveau" element={<ArticleCreationForm />} />
-      <Route path="categories" element={<div>Gestion des catégories</div>} />
+      <Route path="categories" element={<CategoryCreationForm />} />
       <Route path="commentaires" element={<div>Gestion des commentaires</div>} />
     </Routes>
   );
